79ed6a11ffb9cdb43f2e25b4b32e05ab905ec6fa
Nicolas Blackburn <nicolas.blackburn.ca@gmail.com>
2025-06-29 01:47:52 +0000

    Ajouté des functions dans le script, tourne en rond

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-function f(x) { return x/2;}
-function g(x) { return (1 + 3*x) / 2; }
-function c(x) { return [f, g][x % 2](x); }
-function finv(x) { return 2*x; }
-function ginv(x) { return (2*x - 1) / 3; }
-function cinv(x) { 
-  return [finv, finv, ginv][x % 3](x); 
+function ng(x) { return x/2;}
+function nh(x) { return (1 + 3*x) / 2; }
+function nc(x) { return [ng, nh][x % 2](x); }
+function nginv(x) { return 2*x; }
+function nhinv(x) { return (2*x - 1) / 3; }
+function ncinv(x) { 
+  return [nginv, nginv, nhinv][x % 3](x); 
 }
-function af([a, b]) { return [a/2, b/2]; }
-function ag([a, b]) { return [c(a), 3*b/2]; }
-function afinv([a, b]) { return [2*a, 2*b]; }
-function aginv([a, b]) { return [cinv(a), 2*b/3]; }
+function g([a, b]) { return [qmult(a, rational(1, 2)), qmult(b, rational(1, 2))]; }
+function h([a, b]) { return [qadd(rational(1, 2), qmult(a, rational(3, 2))), qmult(b, rational(3, 2))]; }
+function tostr([a, [bn, bd]]) {
+  const aa = qtostr(a);
+  if (aa === "0" && bn === 0) {
+    return "0";
+  } else {
+    return (aa !== "0" ? aa + " + " : "") + (bn === 0 ? "0" : bn === bd ? "x" : bn === 1 ? "x/" + bd : bn + "x/" + bd); 
+  }
+}
+function fix([a, b]) {
+  return qdiv(a, qsub(rational(1), b));
+}
+function cg(x) {
+  return [0, 1, ...x.map(x => x + 1)];
+}
+function ch(x) {
+  return x.map((x, i) => i % 2 ? x + 1 : x);
+}
+function creduce(x) {
+  const v = [[0, 1], [0, 1]];
+  for (let i = 0; i + 1 < x.length; i += 2) {
+    const a = x[i];
+    const b = x[i + 1];
+    if (i < x.length - 2) {
+      v[0] = qadd(v[0], [3**a, 2**b]);
+    } else {
+      v[1] 
+    }
+  }
+}
+function ctostr2(x) {
+  let s = '';
+  for (let i = 0; i + 1 < x.length; i += 2) {
+    const a = x[i];
+    const b = x[i + 1];
+    s += (s.length ? ' + ' : '') + `${a === 0 && b === 0 ? '1' : (a === 0 ? '1' : a === 1 ? '3' : '3' + exponentnumber(a)) + '/' + (b === 0 ? '1' : b === 1 ? '2' : '2' + exponentnumber(b))}`;
+    if (i + 1 === x.length - 1) {
+      s += ' x';
+    }
+  }
+  return s;
+}
+function exponentnumber(n) {
+  return [...(n).toString()].map(d => ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'][parseInt(d)]).join('');
+}
+//function vg([a, b]) { return [a/2, b/2]; }
+//function vh([a, b]) { return [nc(a), 3*b/2]; }
+//function vginv([a, b]) { return [2*a, 2*b]; }
+//function vhinv([a, b]) { return [ncinv(a), 2*b/3]; }
 function compose(...fs) {
   return x => fs
     .reverse()
@@ -36,21 +82,75 @@ function maxfixpoint(a, b = minevenorder(a)) {
     / 2
     / (1 - (3/2)**a/2**b);
 }
+function gcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+function rational(n, d = 1) {
+  const r = [n, d];
+  const e = gcd(r[0], r[1]);
+  return [r[0] / e, r[1] / e];
+}
+function qadd([a, b], [c, d]) {
+  const r = [a*d + b*c, b*d];
+  const e = gcd(r[0], r[1]);
+  return [r[0] / e, r[1] / e];
+}
+function qsub([a, b], [c, d]) {
+  const r = [a*d - b*c, b*d];
+  const e = gcd(...r);
+  return [r[0] / e, r[1] / e];
+}
+function qmult([a, b], [c, d]) {
+  const r = [a*c, b*d];
+  const e = gcd(...r);
+  return [r[0] / e, r[1] / e];
+}
+function qdiv([a, b], [c, d]) {
+  const r = [a*d, b*c];
+  const e = gcd(...r);
+  return [r[0] / e, r[1] / e];
+}
+function qtostr([a, b]) {
+  if (a === 0) {
+    return "0";
+  } else if (a === b) {
+    return "1";
+  } else {
+    return `${a}/${b}`;
+  }
+}
 
 console.table(
   range(20).map(
     i => 
-      (x => 
-        (a =>
-          ({
-            "36n+8": x, 
-            "%3": ginv(ginv(x)) % 3,
-            "α": a,
-            "ω": minevenorder(a),
-            fix: maxfixpoint(a)
-          })
-        )(minoddorder(x))
-      )(36*i+8)
+    (x => 
+      (a =>
+        ({
+          "36n+8": x, 
+          "%3": nginv(nginv(x)) % 3,
+          "α": a,
+          "ω": minevenorder(a),
+          fix: maxfixpoint(a)
+        })
+      )(minoddorder(x))
+    )(36*i+8)
+  )
+)
+
+console.table(
+  range(20).map(
+    omega => {
+      const alpha = minevenorder(omega); 
+      return {
+        "ω": omega,
+        "α": alpha,
+        "n": omega + alpha
+      };
+    }
   )
 )
 
